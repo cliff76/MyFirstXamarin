1a05aa26aab7014fb588e4964338c5a8e7150c56
Clifton Craig <clifton.craig@ge.com>
2017-10-01 14:26:19 -0700

    Creates HelloWorld project.

---
+++ HelloWorld/HelloWorldPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class HelloWorldPage : ContentPage
+    {
+        public HelloWorldPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Ouch!", "You clicked me.", "Go On.");
+        }
+    }
+}
